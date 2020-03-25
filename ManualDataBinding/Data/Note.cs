be631cfc1500aa93a8c4dad2ad4e90e38635d48f
noswad7898 <dawsfield@gmail.com>
2020-03-25 13:02:00 -0500

    Finnished assignment

--- ManualDataBinding/Data/Note.cs
+++ ManualDataBinding/Data/Note.cs
@@ -7,14 +7,35 @@ namespace ManualDataBinding.Data
     /// </summary>
     public class Note
     {
+        public event EventHandler NoteChanged;
         /// <summary>
         /// The title of the Note
         /// </summary>
-        public string Title { get; set; } = DateTime.Now.ToString();
+        private string title = DateTime.Now.ToString();
+        public string Title 
+        { 
+            get { return title; }
+            set 
+            {
+                if (title == value) return;
+                title = value;
+                NoteChanged?.Invoke(this, new EventArgs());
+            } 
+        }
 
         /// <summary>
         /// The text of the note
         /// </summary>
-        public string Body { get; set; } = "";
+        private string body = "";
+        public string Body 
+        {
+            get { return body; }
+            set
+            {
+                if (body == value) return;
+                body = value;
+                NoteChanged?.Invoke(this, new EventArgs());
+            }
+        }
     }
 }
--- ManualDataBinding/MainWindow.xaml.cs
+++ ManualDataBinding/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ManualDataBinding.Data;
 
 namespace ManualDataBinding
 {
@@ -20,9 +21,45 @@ namespace ManualDataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        // create a new not to add to editor
+        Note note = new Note();
+
         public MainWindow()
         {
             InitializeComponent();
+            Editor.Note = note;
+        }
+        /// <summary>
+        /// handler to creane a new note and place it in the editor
+        /// </summary>
+        /// <param name="sender"> button clicked </param>
+        /// <param name="e"> event args </param>
+        public void OnNewNote (object sender, RoutedEventArgs e)
+        {
+            note = new Note();
+            Editor.Note = note;
+
+        }
+        /// <summary>
+        /// event handler for Clear note button
+        /// </summary>
+        /// <param name="sender"> button clicked </param>
+        /// <param name="e"> event args </param>
+        public void OnClearNote (object sender, RoutedEventArgs e)
+        {
+            note.Body = "";
+
+        }
+        /// <summary>
+        /// handler to mutate the current note 
+        /// </summary>
+        /// <param name="sender"> button clicked </param>
+        /// <param name="e"> event args </param>
+        public void OnMutateNote (object sender, RoutedEventArgs e)
+        {
+            note.Title = "M. Gustave H.";
+            note.Body = "Rudeness is merely the expression of fear. People fear they won't get what they want. The most dreadful and unattractive person only needs to be loved and they will open up like a flower.";
+
         }
     }
 }
